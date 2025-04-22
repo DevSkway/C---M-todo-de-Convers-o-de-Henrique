3b687e07a808f38ed2c1f304314803f79a616809
Skway <livesg815@gmail.com>
2025-04-21 23:13:41 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,34 @@
-#include <iostream>
+// Método de Henrique
 
-int ConvertToDecimal(unsigned int& Binary) {
-    const std::string& BinaryString = std::to_string(Binary);
+#include <iostream>
+#include <regex>
+
+bool isBinary(const std::string& Binary) {
+    std::regex regexMatch("^[01]+$");
+    return std::regex_match(Binary, regexMatch);
+}
+
+int ConvertToDecimal(const std::string& Binary) {
+    if (!isBinary(Binary)) {
+        throw std::invalid_argument(Binary + " is not a valid binary string.");
+    }
+
     unsigned int result {0};
 
-    for (int Index = 0; Index <= BinaryString.length(); Index++) {
-        switch (BinaryString[Index]) {
-            case '0': {
-                result *= 2;
-            }
-
-            case '1': {
-                result *= 2;
-                result++;
-            }
+    for (char bit : Binary) {
+        result *= 2;
+        
+        if (bit == '1') {
+            result++;
         }
     }
+
+    return result;
 }
 
 int main() {
-    unsigned int Binary = 101;
+    std::string Binary {"10101011101010101001101110101010111001"};
     
-    std::cout << ConvertToDecimal(Binary);
+    std::cout << "Binario: " << Binary << '\n';
+    std::cout << "Decimal: " << ConvertToDecimal(Binary) << '\n'; 
 }
